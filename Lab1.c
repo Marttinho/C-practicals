01e964d09a48ec0ac8e13824ccb547495b1d08fc
Martin Nahalka <martin@oa-edu-191-191.wireless.abdn.ac.uk>
2017-10-03 17:34:53 +0100

    Lab1 in progress, addition, multiplication tested

--- Lab1.c
+++ Lab1.c
@@ -1,69 +1,81 @@
 #include <stdio.h>      /* Standard Library of Input and Output */
-#include <complex.h>    /* Standard Library of Complex Numbers */
 
 
-void addition(_Complex double z1,_Complex double z2)
+
+void addition(double real1,double real2,double imag1,double imag2)
 {
-  printf("in function\n");
-  _Complex double sum = z1 + z2;
-  printf("The sum: Z1 + Z2 = %.2f %+.2fi\n", creal(sum), cimag(sum));
+  printf("in addition function\n");
+  double rsum = real1 + real2;
+  double isum = imag1 + imag2;
+  printf("The sum: %.2f%.2fi + %.2f%.2fi = %.2f %+.2fi\n",real1,imag1,real2,imag2,rsum,isum);
+}//this one works
+
+void substraction(double real1,double real2,double imag1,double imag2)
+{
+  printf("in substraction function\n");
+  double rsum = real1 - real2;
+  double isum = imag1 - imag2;
+  printf("The sum: %.2f%.2fi - %.2f%.2fi = %.2f %+.2fi\n",real1,imag1,real2,imag2,rsum,isum);
+}//working
+
+void multiplication(double real1,double real2,double imag1,double imag2)
+{
+  printf("in multiplication function\n");
+  double rsum = real1 * real2 - imag1 * imag2;
+  double isum = imag1 * real2 + real1 * imag2;
+  printf("The sum: %.2f%.2fi * %.2f%.2fi = %.2f %+.2fi\n",real1,imag1,real2,imag2,rsum,isum);
 }
 
-void substraction(_Complex double z1, _Complex double z2)
+void division(double real1,double real2,double imag1,double imag2)
 {
-  _Complex double sum = z1 - z2;
-  printf("The sum: Z1 - Z2 = %.2f %+.2fi\n", creal(sum), cimag(sum));
-}
-
-void multiplication(_Complex double z1, _Complex double z2)
-{
-  _Complex double sum = z1 * z2;
-  printf("The sum: Z1 * Z2 = %.2f %+.2fi\n", creal(sum), cimag(sum));
-}
-
-void division(_Complex double z1, _Complex double z2)
-{
-  _Complex double sum = z1 / z2;
-  printf("The sum: Z1 / Z2 = %.2f %+.2fi\n", creal(sum), cimag(sum));
+  printf("in division function\n");
+  double rsum = real1 / real2;
+  double isum = imag1 / imag2;
+  printf("The sum: %.2f%.2fi / %.2f%.2fi = %.2f %+.2fi\n",real1,imag1,real2,imag2,rsum,isum);
 }
 
 int main(int argc, char const *argv[])
 {
 
-    double real1=2,real2=2,imag1=2,imag2=2;
+    double real1,real2,imag1,imag2;
     int choice;
+    char sign1[1],sign2[1];
 
-    printf("what would you like to do? \n + - / * \n");
+    printf("what would you like to do? \n + - * / \n");
 
     scanf("%d", &choice);
 
-    /*printf("Now enter the complex numbers\nenter the real part\n" );
+    printf("Now enter the complex numbers\nenter the real part\n" );
     scanf("%lf", &real1);
+    /*printf("now enter the + or - for your first complex number\n" );
+    scanf("%c", &sign1);*/
     printf("now enter the imaginary part of the first number\n" );
     scanf("%lf", &imag1);
     printf("Now enter the 2nd complex number\nenter the real part\n" );
     scanf("%lf", &real2);
+    /*printf("now enter the + or - for your second complex number\n" );
+    scanf("%c", &sign2);*/
     printf("now enter the imaginary part of the second number\n" );
-    scanf("%lf", &imag2);*/
+    scanf("%lf", &imag2);
 
 
-    _Complex double z1 = real1 + imag1 * I;
-    _Complex double z2 = real2 - imag2 * I;
+    //_Complex double z1 = real1 sign1 imag1 * I;
+    //_Complex double z2 = real2 sign2 imag2 * I;
     printf("choice is %d\n", choice);
     switch (choice)
     {
       case 1:
         printf("here\n");
-        addition(z1, z2);
+        addition(real1, real2, imag1, imag2);
         break;
       case 2:
-        substraction(z1, z2);
+        substraction(real1, real2, imag1, imag2);
         break;
       case 3:
-        multiplication(z1, z2);
+        multiplication(real1, real2, imag1, imag2);
         break;
       case 4:
-        division( z1, z2);
+        division(real1, real2, imag1, imag2);
         break;
     }
 
