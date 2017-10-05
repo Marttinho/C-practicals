fc507e9ca376aebce1d23d67217782a118216073
Martin Nahalka <martin@as-macbook-pro.home>
2017-10-05 22:41:26 +0100

    checks input

--- Lab1.c
+++ Lab1.c
@@ -1,5 +1,5 @@
 //By Martin Nahalka, 51444972
-#include <stdio.h>      
+#include <stdio.h>
 #include <stdlib.h>
 
 
@@ -36,43 +36,80 @@ void division(double real1,double real2,double imag1,double imag2)
   printf("The sum: %.2f%+.2fi / %.2f%+.2fi = %.2f%+.2fi\n",real1,imag1,real2,imag2,rsum,isum);
 }//function handling division of complex numbers
 
+void inputf(int choice)
+{
+  char term;
+  double real1,real2,imag1,imag2;
+  printf("Now enter the complex numbers\nenter the real part\n" );
+  //scanf("%lf", &real1);
+  if(scanf("%lf%c", &real1, &term) != 2 || term != '\n')
+  {
+    printf("Wrong input, try again\n" );
+    fseek(stdin,0,SEEK_END);
+    inputf(choice);
+  }
+  printf("now enter the imaginary part of the first number\n" );
+  //scanf("%lf", &imag1);
+  if(scanf("%lf%c", &imag1, &term) != 2 || term != '\n')
+  {
+    printf("Wrong input, try again\n" );
+    fseek(stdin,0,SEEK_END);
+    inputf(choice);
+  }
+  printf("Now enter the 2nd complex number\nenter the real part\n" );
+  //scanf("%lf", &real2);
+  if(scanf("%lf%c", &real2, &term) != 2 || term != '\n')
+  {
+    printf("Wrong input, try again\n" );
+    fseek(stdin,0,SEEK_END);
+    inputf(choice);
+  }
+  printf("now enter the imaginary part of the second number\n" );
+  //scanf("%lf", &imag2);
+  if(scanf("%lf%c", &imag2, &term) != 2 || term != '\n')
+  {
+    printf("Wrong input, try again\n" );
+    fseek(stdin,0,SEEK_END);
+    inputf(choice);
+  }
+
+  //switch statement for determining what the user wants to do
+  switch (choice)
+    {
+    case 1:
+      addition(real1, real2, imag1, imag2);
+      break;
+    case 2:
+      substraction(real1, real2, imag1, imag2);
+      break;
+    case 3:
+      multiplication(real1, real2, imag1, imag2);
+      break;
+    case 4:
+      division(real1, real2, imag1, imag2);
+      break;
+    }
+
+
+}
+
+
 int main(int argc, char const *argv[])
 {
-    double real1,real2,imag1,imag2;
     int choice;
 
     printf("what would you like to do? \n-press 1 for addition\n-press 2 for substraction\n-press 3 for multiplication\n-press 4 for division\n");
 
-    if (scanf("%d", &choice) != (1 | 2 | 3 | 4))
+    if (scanf("%d", &choice) != (1 || 2 || 3 || 4))
     {
-      printf("you need to enter 1,2,3 or 4\nexiting\n" );
+      printf("you need to enter 1,2,3 or 4\n" );
+      fseek(stdin,0,SEEK_END);
+      main(argc, argv);
+
     }
     else
     {
-    printf("Now enter the complex numbers\nenter the real part\n" );
-    scanf("%lf", &real1);
-    printf("now enter the imaginary part of the first number\n" );
-    scanf("%lf", &imag1);
-    printf("Now enter the 2nd complex number\nenter the real part\n" );
-    scanf("%lf", &real2);
-    printf("now enter the imaginary part of the second number\n" );
-    scanf("%lf", &imag2);
-    //switch statement for determining what the user wants to do
-    switch (choice)
-      {
-      case 1:
-        addition(real1, real2, imag1, imag2);
-        break;
-      case 2:
-        substraction(real1, real2, imag1, imag2);
-        break;
-      case 3:
-        multiplication(real1, real2, imag1, imag2);
-        break;
-      case 4:
-        division(real1, real2, imag1, imag2);
-        break;
-      }
+      inputf(choice);
     }
 
     return 0;
